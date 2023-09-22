fb3830310696a66256290fedf0384b4c2cb57768
sakshi goyal <sakshigoyal7351@gmail.com>
2023-09-22 18:19:09 +0530

    first commit

---
+++ ExcelR-springboot-backend/src/main/java/com/sakshi/ExcelRSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sakshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelRSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelRSpringbootBackendApplication.class, args);
+	}
+
+}
